7dcd85e77fa94401c9c422c5ff6fa4e23fcface5
moonihun <anslgns1@gmail.com>
2021-11-26 09:59:59 +0900

    initial commit

--- js/background.js
+++ js/background.js
@@ -2,10 +2,9 @@ const images = ['0.jpeg', '1.jpeg', '2.jpeg'];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement('img');
+// const bgImage = document.createElement('img');
 
-bgImage.src = `img/${chosenImage}`;
-
-console.log(bgImage);
+const randomImage = `img/${chosenImage}`;
+document.body.style.backgroundImage = 'url("' + randomImage + '")';
 
 document.body.appendChild(bgImage);
